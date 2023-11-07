edc6cb69ad002023cdb00f65897d68f2de13c1d3
AkobirToshtemirov <toshtemirovakobir8@gmail.com>
2023-11-07 18:56:28 +0500

    Code has been reviewed according to mentors feedback and changed.

--- src/main/java/org/epam/utils/StringUtils.java
+++ src/main/java/org/epam/utils/StringUtils.java
@@ -1,7 +1,13 @@
 package org.epam.utils;
 
+import org.apache.commons.lang3.math.NumberUtils;
+
 public class StringUtils {
     public static boolean isPositiveNumber(String str) {
-        return org.apache.commons.lang3.StringUtils.isNumeric(str) && Integer.parseInt(str) > 0;
+        if (NumberUtils.isCreatable(str)) {
+            Number parsedNumber = NumberUtils.createNumber(str);
+            return parsedNumber.doubleValue() > 0;
+        }
+        return false;
     }
 }
--- src/test/java/StringUtilsTest.java
+++ src/test/java/StringUtilsTest.java
@@ -3,9 +3,12 @@ import org.epam.utils.StringUtils;
 
 import static org.junit.jupiter.api.Assertions.*;
 public class StringUtilsTest {
+    /* remove the test from the method name */
     @Test
     public void testIsPositiveNumber() {
         assertTrue(StringUtils.isPositiveNumber("42"));
+        assertTrue(StringUtils.isPositiveNumber("43.5"));
+        assertFalse(StringUtils.isPositiveNumber("-43.5"));
         assertFalse(StringUtils.isPositiveNumber("-42"));
         assertFalse(StringUtils.isPositiveNumber("notanumber"));
     }
